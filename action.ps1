cffcfb01b6fb179bffedeb1be366b95deb3e236e
Jeff Lill <jeff@lilltek.com>
2021-05-26 20:43:07 -0700

    scripted push

--- action.ps1
+++ action.ps1
@@ -171,7 +171,7 @@ try
     $scriptPath = [System.IO.Path]::Combine($nfRoot, "Images", "publish.ps1")
 
     Write-ActionOutput "Building container images"
-    pwsh -File $scriptPath -NonInteractive $allOption $baseOption $otherOption $serviceOption $testOption $noPruneOption $noPushOption *> $buildLog
+    pwsh -File $scriptPath -NonInteractive $allOption $baseOption $otherOption $serviceOption $testOption $noPruneOption $noPushOption 6>&1 2>&1 > $buildLog
     ThrowOnExitCode
 
     # Make all of the public images public when requested 
